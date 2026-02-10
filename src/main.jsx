da4ad07a26dea4e8dca7e10e99a535cbaa0fe489
manuelwestermeier <westermeier111@gmail.com>
2026-02-10 21:26:14 +0100

    added white mode

--- src/main.jsx
+++ src/main.jsx
@@ -24,3 +24,32 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Main />
   </>, //</React.StrictMode>,
 );
+
+if (document.location.hash == "#white") {
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    `<style>:root {
+  /* Backgrounds */
+  --b: rgb(245, 247, 250) !important;          /* Haupt-Hintergrund (sehr hell) */
+  --b-l: rgba(245, 247, 250, 0.8)!important;  /* Transparente Variante */
+  --b-light: rgb(255, 255, 255)!important;    /* Karten / Panels */
+
+  /* Content / Text */
+  --c: rgb(20, 25, 30)!important;             /* Haupt-Textfarbe (dunkel) */
+
+  /* Accent */
+  --a: rgb(157, 247, 255)!important;           /* DodgerBlue, gut lesbar im White Mode */
+}
+
+.bg-image {
+ background-image:
+    linear-gradient(to bottom, var(--b), transparent),
+    linear-gradient(to bottom, transparent, var(--b)),
+    url("/bg.jpg");
+  background-size: cover;
+  background-position: center;
+  filter: brightness(1);
+}
+</style>`,
+  );
+}
