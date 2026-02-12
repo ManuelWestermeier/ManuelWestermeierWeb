61beff82ed8757994e936ab041b15dfdbfa1f3df
ManuelWestermeier <westermeier111@gmail.com>
2026-02-12 20:06:22 +0100

    better text view

--- src/comp/write-text/index.jsx
+++ src/comp/write-text/index.jsx
@@ -1,8 +1,37 @@
-import React from "react";
-import SplitText from "../react-bits/split-text.jsx";
+import React, { useEffect, useState } from "react";
+import "./index.css";
 
-function WriteText({ text = "" }) {
-  return <SplitText text={text} tag="span" />;
+function WriteText({ text = "", timeToNextChar = 100, delay = 0 }) {
+  const [current, setCurrent] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      const chars = text.split("");
+
+      var interval = setInterval(() => {
+        const nextChar = chars.shift();
+
+        if (!nextChar) {
+          return clearInterval(interval);
+        }
+
+        setCurrent((old) => old + nextChar);
+      }, timeToNextChar);
+    }, delay);
+    return () => true;
+  }, []);
+
+  return current.split("").map((char, i) => {
+    const style = {
+      animation: `text-fade-in ${timeToNextChar * i}ms ease-in-out`,
+    };
+
+    return (
+      <span style={style} key={i}>
+        {char}
+      </span>
+    );
+  });
 }
 
 export default WriteText;
